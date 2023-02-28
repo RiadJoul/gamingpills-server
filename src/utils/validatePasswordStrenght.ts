5d601e1532f9e5212d0470e21d1b875e80a26e36
Joul Riad, SUB-SDC-NL-DOS-OCD <Riad.Joul@swisscom.com>
2023-02-28 10:52:53 +0100

    GP-2 fixed

--- src/utils/validatePasswordStrenght.ts
+++ src/utils/validatePasswordStrenght.ts
@@ -1,5 +1,8 @@
 export const validatePassword = (password: string) => {
-  if (password.match(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/)) return true; //Minimum eight characters, at least one letter and one number
-
+  //Minimum eight characters
+  if (password.length >= 8) {
+    return true;
+  }
+  
   return false;
 };
